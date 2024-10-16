ebaf8078d5db9197b157095e70fdac4b9ce82290
Kyaw Monkey <kyaw.codes@gmail.com>
2024-10-16 14:29:48 +0700

    chore: finish state for todo card

--- app/src/main/java/com/kyaw/todolist/screens/TodoListScreen.kt
+++ app/src/main/java/com/kyaw/todolist/screens/TodoListScreen.kt
@@ -23,7 +23,6 @@ import androidx.compose.material.icons.outlined.KeyboardArrowDown
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -33,11 +32,14 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -186,12 +188,13 @@ fun TodoSection(modifier: Modifier = Modifier, title: String) {
 
 @Composable
 fun TodoItem(modifier: Modifier = Modifier, todo: Todo) {
-    Row(modifier = modifier
-        .fillMaxWidth()
-        .padding(5.dp)
-        .clip(RoundedCornerShape(12.dp))
-        .background(Color.White)
-        .padding(12.dp),
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(5.dp)
+            .clip(RoundedCornerShape(12.dp))
+            .background(Color.White)
+            .padding(12.dp).alpha(if (todo.finished) 0.4f else 1f),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Icon(
@@ -206,7 +209,15 @@ fun TodoItem(modifier: Modifier = Modifier, todo: Todo) {
             tint = primaryLight
         )
         Spacer(Modifier.width(4.dp))
-        Text(todo.name, fontWeight = FontWeight.Medium, fontSize = 14.sp, color = primaryLight)
+        Text(
+            todo.name,
+            fontWeight = FontWeight.Medium,
+            fontSize = 14.sp,
+            color = primaryLight,
+            style = TextStyle.Default.copy(
+                textDecoration = if (todo.finished) TextDecoration.LineThrough else TextDecoration.None
+            )
+        )
     }
 }
 
